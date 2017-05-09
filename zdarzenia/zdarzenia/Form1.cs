4f6a6519a878f810da44760f04b0f4d2a0e416f4
bajtek94 <damimiko94@gmail.com>
2017-05-09 13:23:50 +0200

    from zad2

---
+++ zdarzenia/zdarzenia/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zdarzenia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyArray tab = new MyArray();
+            tab.Add(1);
+            tab.Add(2);
+            tab.Add(5);
+            tab.Add(54);
+            tab.Add(523);
+            tab.Add(235);
+            tab.Add(615);
+            tab.Add(235);
+            tab.Add(615);
+            tab.Add(35);
+            tab.Add(65);
+            tab.Add(125);
+            tab[634] = 43;
+            try
+            {
+                Console.WriteLine(tab[635]);
+            }
+            catch
+            {
+                Console.WriteLine("Błąd: Zły indeks");
+            }
+            Console.WriteLine(tab[632]);
+            Console.WriteLine(tab[32]);
+            Console.WriteLine(tab[156]);
+            Console.WriteLine(tab[9]);
+            tab.Add(23);
+            Console.WriteLine(tab[635]);
+        }
+
+
+
+    }
+}
---
+++ zdarzenia/zdarzenia/MyArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zdarzenia
+{
+    class MyArray
+    {
+        int[] tab;
+        int countOfElements;
+
+        public MyArray()
+        {
+            countOfElements = 0;
+            tab = new int[4];
+        }
+
+        public int this[int el]
+        {
+            get
+            {
+                if (el < countOfElements)
+                {
+                    return tab[el];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                if (el >= countOfElements)
+                {
+                    Console.WriteLine("Dodawanie elementu poza tablicą.");
+                    growTab(el + 1);
+                    countOfElements = el + 1;
+                }
+                tab[el] = value;
+            }
+        }
+
+        public void Add(int el)
+        {
+            int count = tab.Count();
+            if (countOfElements >= count)
+            {
+                growTab(2 * count);
+            }
+            tab[countOfElements] = el;
+            countOfElements++;
+            Console.WriteLine("dodano liczbę " + el);
+            Console.WriteLine("aktualna ilość danych w tablicy: " + countOfElements);
+        }
+
+        private void growTab(int newSize)
+        {
+            Console.WriteLine("Powiększanie tablicy ...");
+            int count = tab.Count();
+            Console.WriteLine("Stary rozmiar: " + count);
+            Array.Resize(ref tab, newSize);
+            Console.WriteLine("Zwiększono rozmiar do: " + tab.Count());
+        }
+    }
+}
